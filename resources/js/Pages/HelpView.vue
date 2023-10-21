d13c63d809f0b1c02fd79ae148b450329f576eb7
ebr-salem <ebrahmedsalem03@gmail.com>
2023-10-21 17:53:00 +0300

    This file should be added with preserve scroll commit, sorry :)

--- resources/js/Pages/HelpView.vue
+++ resources/js/Pages/HelpView.vue
@@ -1,10 +1,23 @@
 <template>
     <h1>This is help page</h1>
     <Nav />
+    <h1 style="margin-top: 500px">The current time is: {{ time }}</h1>
+    <Link
+        href="/help"
+        as="button"
+        preserve-scroll
+    >
+        Refresh
+    </Link>
 </template>
 
 <script setup>
 import Nav from "./shared/Nav.vue";
+import { Link } from "@inertiajs/vue3";
+
+defineProps({
+    time: String,
+});
 </script>
 
 <style>
